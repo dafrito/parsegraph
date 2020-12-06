5a1c13bf79eafbc1b8e838ad80d5c528cba7eeb6
Aaron Faanes <dafrito@gmail.com>
2020-12-05 21:15:47 -0600

    Export to this for use in node program

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "pagingbuffer.js",
+    globalObject: "this",
     library: "PagingBuffer",
     libraryTarget: "umd",
   },
