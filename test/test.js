8e99bd711b067723f7c81f28a3b24510a60deb65
Aaron Faanes <dafrito@gmail.com>
2020-12-05 21:40:34 -0600

    Add appendData test

--- test/test.js
+++ test/test.js
@@ -39,4 +39,16 @@ describe('PagingBuffer', function() {
       });
     });
   });
+  describe('#appendData()', function() {
+    it('works for values', function() {
+        var canvas = new HTMLCanvasElement( 500, 500 );
+        var gl = canvas.getContext('webgl');
+        var pb = new PagingBuffer(gl, 1);
+        pb.addPage(()=>{}, null);
+        const a_position = pb.defineAttrib("a_position", 3);
+        pb.appendData(a_position, 1, 0, 0);
+        pb.appendData(a_position, 0, 1, 0);
+        pb.appendData(a_position, 0, 0, 1);
+    });
+  });
 });
