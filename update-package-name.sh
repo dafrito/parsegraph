e7eb4bcade7a9f0edd406e537e409e910cad0ec1
Aaron Faanes <dafrito@gmail.com>
2020-12-10 04:15:40 -0600

    Make update-package-name smarter with how it gets its package name

--- update-package-name.sh
+++ update-package-name.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-sed -i "s/microproject/$1/" .git/config
-git ls-files | xargs sed -i "s/TODO-PACKAGE-NAME/$1/g" && echo Now run git rm -f $0 to remove this file.
+PACKAGE_NAME=$1
+if test z"$PACKAGE_NAME" = z; then
+  DEFAULT_NAME=`basename $(pwd)`
+  echo -n "Enter the package name (blank for $DEFAULT_NAME):"
+  read PACKAGE_NAME
 
+  if test z"$PACKAGE_NAME" = z; then
+    PACKAGE_NAME=$DEFAULT_NAME
+  fi
+fi
+echo package name is $PACKAGE_NAME
 
-
+sed -i "s/microproject/$PACKAGE_NAME/g" .git/config
+git ls-files | xargs sed -i "s/TODO-PACKAGE-NAME/$PACKAGE_NAME/g" && echo Now run git rm -f $0 to remove this file.
