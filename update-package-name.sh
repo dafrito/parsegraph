dcb2d13687050f90c1c9fa2d62a0ef4cd8c32812
Aaron Faanes <dafrito@gmail.com>
2020-12-09 23:02:49 -0600

    Fix update-package-name

--- update-package-name.sh
+++ update-package-name.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-git ls-files | xargs sed -i "s/TODO-PACKAGE-NAME/$1/"
+sed -i "s/microproject/$1/" .git/config
+git ls-files | xargs sed -i "s/TODO-PACKAGE-NAME/$1/" && echo You may now remove $0.
+
+
+
