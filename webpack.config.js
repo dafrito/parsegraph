1548b7a32cf61afc724a1bc503d0199b9472b106
Aaron Faanes <dafrito@gmail.com>
2022-02-11 18:41:20 -0600

    Fix webpack name

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "parsegraph-layout.js",
     globalObject: "this",
-    library: "parsegraph",
+    library: "parsegraph_layout",
     libraryTarget: "umd",
   },
   module: {
