b722684b8405a4bbb944a73605ff0e734d44fc24
Aaron Faanes <dafrito@gmail.com>
2022-02-26 16:40:58 -0600

    Upgrade webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,9 @@
-const path = require("path");
+const {webpackConfig, relDir} = require("./webpack.common");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/index.ts"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "parsegraph-layout.js",
-    globalObject: "this",
-    library: "parsegraph_layout",
-    libraryTarget: "umd",
+  entry: {
+    index: relDir("src/index.ts"),
+    demo: relDir("src/demo.ts"),
   },
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts|tsx?)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'ts-loader']
-      },
-      {
-        test: /\.(glsl|vs|fs|vert|frag)$/,
-        exclude: /node_modules/,
-        use: ["ts-shader-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".ts", ".tsx", ".glsl"],
-    modules: [path.resolve(__dirname, "src"), "node_modules"],
-  },
-  mode: "development",
-  devtool: "eval-source-map",
+  ...webpackConfig(false),
 };
