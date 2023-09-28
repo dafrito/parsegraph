0d52ee71f0d34d10ada48b64d1ef144a174ac409
Aaron Faanes <dafrito@gmail.com>
2023-09-27 21:19:03 -0700

    add missing separation values

--- test/test.ts
+++ test/test.ts
@@ -68,6 +68,8 @@ interface BlockStyle {
   borderThickness: number;
   verticalPadding: number;
   horizontalPadding: number;
+  verticalSeparation: number;
+  horizontalSeparation: number;
 };
 
 const BLOCK_STYLE = {
@@ -75,7 +77,9 @@ const BLOCK_STYLE = {
   minHeight: 100,
   borderThickness: 4,
   verticalPadding: 2,
-  horizontalPadding: 4
+  horizontalPadding: 4,
+  horizontalSeparation: 10,
+  verticalSeparation: 10
 }
 
 const BUD_STYLE = {
@@ -83,7 +87,9 @@ const BUD_STYLE = {
   minHeight: 30,
   borderThickness: 4,
   verticalPadding: 4,
-  horizontalPadding: 4
+  horizontalPadding: 4,
+  horizontalSeparation: 10,
+  verticalSeparation: 10
 }
 
 const EMPTY_STYLE = {
@@ -91,7 +97,9 @@ const EMPTY_STYLE = {
   minHeight: 100,
   borderThickness: 0,
   verticalPadding: 0,
-  horizontalPadding: 0
+  horizontalPadding: 0,
+  horizontalSeparation: 10,
+  verticalSeparation: 10
 }
 
 const readStyle = (given?: any): BlockStyle => {
